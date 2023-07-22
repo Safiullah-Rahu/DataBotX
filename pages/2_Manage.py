66c20299d12f9eb095fd4c7bd1ca736ecd1f2aaa
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-07-22 20:42:34 +0500

    Update 2_Manage.py

--- pages/2_Manage.py
+++ pages/2_Manage.py
@@ -63,95 +63,96 @@ def click_button():
 
 # Initialize Pinecone with API key and environment
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
-
-# Checkbox for the first option to document upload
-first_opt = st.checkbox('Create New Index to Upload Documents')
-# Checkbox for the second option to document upload
-second_opt = st.checkbox('Use Existing Index to Upload Documents')
-# Checkbox for the third option to delete existing indexes
-third_opt = st.checkbox('Delete Any Existing Index')
-
-st.write("---")
-
-if first_opt:
-    # Prompt the user to upload PDF/TXT/XLSX files
-    st.write("Upload PDF/TXT/XLSX Files:")
-    uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "xlsx"], label_visibility="collapsed", accept_multiple_files = True)
-
-    if uploaded_files != []:
-        documents = load_docs(uploaded_files)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        docs = text_splitter.create_documents(documents)
-
-        # Initialize OpenAI embeddings
-        embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
-
-        # Display the uploaded file content
-        file_container = st.expander(f"Click here to see your uploaded content:")
-        file_container.write(docs)
-
-        # Display success message
-        st.success("Document Loaded Successfully!")
-        pinecone_index = st.text_input("Enter the name of Index: ")
-        if pinecone_index != "":
-            st.info('Initializing Index Creation...')
-            # Create a new Pinecone index
-            pinecone.create_index(
-                    name=pinecone_index,
-                    metric='cosine',
-                    dimension=1536  # 1536 dim of text-embedding-ada-002
-                    )
-            st.success('Index Successfully Created!')
-            time.sleep(80)
-            st.info('Initializing Document Uploading to DB...')
-            # Upload documents to the Pinecone index
-            vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
-            
+passme = st.sidebar.text_input("Write Password:")
+if passme == "manageme":
+    # Checkbox for the first option to document upload
+    first_opt = st.checkbox('Create New Index to Upload Documents')
+    # Checkbox for the second option to document upload
+    second_opt = st.checkbox('Use Existing Index to Upload Documents')
+    # Checkbox for the third option to delete existing indexes
+    third_opt = st.checkbox('Delete Any Existing Index')
+    
+    st.write("---")
+    
+    if first_opt:
+        # Prompt the user to upload PDF/TXT/XLSX files
+        st.write("Upload PDF/TXT/XLSX Files:")
+        uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "xlsx"], label_visibility="collapsed", accept_multiple_files = True)
+    
+        if uploaded_files != []:
+            documents = load_docs(uploaded_files)
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs = text_splitter.create_documents(documents)
+    
+            # Initialize OpenAI embeddings
+            embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
+    
+            # Display the uploaded file content
+            file_container = st.expander(f"Click here to see your uploaded content:")
+            file_container.write(docs)
+    
             # Display success message
-            st.success("Document Uploaded Successfully!")
-
-elif second_opt:
-    # Prompt the user to upload PDF/TXT/XLSX files
-    st.write("Upload PDF/TXT/XLSX Files:")
-    uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "xlsx"], label_visibility="collapsed", accept_multiple_files = True)
-
-    if uploaded_files != []:
-        documents = load_docs(uploaded_files)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        docs = text_splitter.create_documents(documents)
-
-        # Initialize OpenAI embeddings
-        embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
-        # Display success message
-        st.success("Document Loaded Successfully!")
-        # Display the uploaded file content
-        file_container = st.expander(f"Click here to see your uploaded content:")
-        file_container.write(docs)
-
+            st.success("Document Loaded Successfully!")
+            pinecone_index = st.text_input("Enter the name of Index: ")
+            if pinecone_index != "":
+                st.info('Initializing Index Creation...')
+                # Create a new Pinecone index
+                pinecone.create_index(
+                        name=pinecone_index,
+                        metric='cosine',
+                        dimension=1536  # 1536 dim of text-embedding-ada-002
+                        )
+                st.success('Index Successfully Created!')
+                time.sleep(80)
+                st.info('Initializing Document Uploading to DB...')
+                # Upload documents to the Pinecone index
+                vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
+                
+                # Display success message
+                st.success("Document Uploaded Successfully!")
+    
+    elif second_opt:
+        # Prompt the user to upload PDF/TXT/XLSX files
+        st.write("Upload PDF/TXT/XLSX Files:")
+        uploaded_files = st.file_uploader("Upload", type=["pdf", "txt", "xlsx"], label_visibility="collapsed", accept_multiple_files = True)
+    
+        if uploaded_files != []:
+            documents = load_docs(uploaded_files)
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs = text_splitter.create_documents(documents)
+    
+            # Initialize OpenAI embeddings
+            embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
+            # Display success message
+            st.success("Document Loaded Successfully!")
+            # Display the uploaded file content
+            file_container = st.expander(f"Click here to see your uploaded content:")
+            file_container.write(docs)
+    
+            time.sleep(10)
+            st.write("Existing Indexes:👇")
+            st.write(pinecone.list_indexes())
+            pinecone_index = st.text_input("Write Name of Existing Index: ")
+            up_check = st.checkbox('Check this to Upload Docs in Selected Index')
+            if up_check:
+                st.info('Initializing Document Uploading to DB...')
+                # Upload documents to the Pinecone index
+                time.sleep(30)
+                vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
+                
+                # Display success message
+                st.success("Document Uploaded Successfully!")
+    
+    elif third_opt:
         time.sleep(10)
         st.write("Existing Indexes:👇")
         st.write(pinecone.list_indexes())
-        pinecone_index = st.text_input("Write Name of Existing Index: ")
-        up_check = st.checkbox('Check this to Upload Docs in Selected Index')
-        if up_check:
-            st.info('Initializing Document Uploading to DB...')
-            # Upload documents to the Pinecone index
-            time.sleep(30)
-            vector_store = Pinecone.from_documents(docs, embeddings, index_name=pinecone_index)
-            
-            # Display success message
-            st.success("Document Uploaded Successfully!")
-
-elif third_opt:
-    time.sleep(10)
-    st.write("Existing Indexes:👇")
-    st.write(pinecone.list_indexes())
-    pinecone_index = st.text_input("Write Name of Existing Index to delete: ")
-    st.write(f"The Index named '{pinecone_index}' is selected for deletion.")
-    del_check = st.checkbox('Check this to Delete Index')
-    if del_check:
-        with st.spinner('Deleting Index...'):
-            time.sleep(5)
-            pinecone.delete_index(pinecone_index)
-            time.sleep(10)
-        st.success(f"'{pinecone_index}' Index Deleted Successfully!")
+        pinecone_index = st.text_input("Write Name of Existing Index to delete: ")
+        st.write(f"The Index named '{pinecone_index}' is selected for deletion.")
+        del_check = st.checkbox('Check this to Delete Index')
+        if del_check:
+            with st.spinner('Deleting Index...'):
+                time.sleep(5)
+                pinecone.delete_index(pinecone_index)
+                time.sleep(10)
+            st.success(f"'{pinecone_index}' Index Deleted Successfully!")
